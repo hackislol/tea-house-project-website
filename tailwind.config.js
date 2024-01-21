af39d9f343db654a48874e7ac5b9d532b0ae43c1
hackislol <fdhalam@gmail.com>
2024-01-21 15:51:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+  }
+
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
